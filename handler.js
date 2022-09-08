9d3193e2eb3d02f3c6629c72c0a6a1b993f70f8a
Ricardo Masabel <r.masabel.a@hotmail.com>
2022-09-08 06:50:51 -0500

    Adición de PendingOrdersQueue

--- handler.js
+++ handler.js
@@ -1,20 +1,39 @@
 'use strict';
 
 const { v1: uuidv1 } = require('uuid');
+const AWS = require('aws-sdk');
 
-module.exports.realizarPedido = async (event) => {
+AWS.config.update({region: process.env.REGION});
+
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const QUEUE_URL = process.env.PENDING_ORDER_QUEUE;
+
+module.exports.realizarPedido = (event, context, callback) => {
   console.log('Se invocó a RealizarPedido');
   const orderId = uuidv1();
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'El pedido fue registrado con orden #: ${orderId}',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+
+	const params = {
+		MessageBody: JSON.stringify({ orderId: orderId }),
+		QueueUrl: QUEUE_URL
+	};
+
+	sqs.sendMessage(params, function(err, data) {
+		if (err) {
+			sendResponse(500, err, callback);
+		} else {
+			const message = {
+				orderId: orderId,
+				messageId: data.MessageId
+			};
+			sendResponse(200, message, callback);
+		}
+	});
 };
+
+function sendResponse(statusCode, message, callback) {
+	const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	callback(null, response);
+}
